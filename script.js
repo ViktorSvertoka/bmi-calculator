95454244733660158be5beaade20d60d14755e7c
Viktor Svertoka <115661003+ViktorSvertoka@users.noreply.github.com>
2023-04-08 12:43:56 +0300

    Fix(JS) fix code, corrected the errors pointed out Moslihbadr
    Co-Authored-By: Badr Moslih <113358493+Moslihbadr@users.noreply.github.com>

--- script.js
+++ script.js
@@ -1,1 +1,36 @@
 //------------BMI-CALCULATOR------------
+const calculateBtn = document.getElementById('calculate');
+const heightInput = document.querySelector('#height');
+const weightInput = document.querySelector('#weight');
+const resultOutput = document.querySelector('#result');
+const commentOutput = document.querySelector('.comment');
+
+calculateBtn.addEventListener('click', function () {
+  const heightValue = Number(heightInput.value);
+  const weightValue = Number(weightInput.value);
+
+  if (isNaN(heightValue) || isNaN(weightValue)) {
+    alert('Please enter valid height and weight values!');
+    return;
+  }
+
+  const heightInMeters = heightValue / 100;
+  const bmiValue = weightValue / (heightInMeters * heightInMeters);
+  const roundedBmiValue = bmiValue.toFixed(2);
+
+  resultOutput.innerHTML = roundedBmiValue;
+
+  let status = '';
+
+  if (bmiValue < 18.5) {
+    status = 'Underweight 😒';
+  } else if (bmiValue < 25) {
+    status = 'Normal Weight 😍';
+  } else if (bmiValue < 30) {
+    status = 'Overweight 😮';
+  } else {
+    status = 'Obese 😱';
+  }
+
+  commentOutput.innerHTML = `Comment: you are <span id="comment">${status}</span>`;
+});
